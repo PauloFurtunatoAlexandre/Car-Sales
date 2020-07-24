e6cd6422563def945982781e7217a72899dc46d6
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-07-24 17:28:51 -0500

    setup the store objects, connected the app component to the store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import { carSalesReducer } from "./reducers/carSalesReducer";
+
+const this_is_a_greate_car_store = createStore(carSalesReducer);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={this_is_a_greate_car_store}>
+    <App />
+  </Provider>,
+  rootElement
+);
